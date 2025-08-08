cd89f5982fd69d398bc4ac7aaf0440777ed6e5e3
14sammie41 <q85e7u2t@students.codeinstitute.net>
2025-08-08 19:15:46 +0100

    Removed all edited through AI code and started the deployment through Heroku process from scratch

--- codestar/urls.py
+++ codestar/urls.py
@@ -21,5 +21,4 @@ from blog.views import my_blog
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', my_blog, name='blog'),
-    path('', my_blog, name='home'),  # Add this line for root URL
 ]
